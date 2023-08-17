0296477431cadd9cc89849a8c4ff9f057ca8a707
Andrew Thomas <andrewthomas@Andrews-Mac-mini.local>
2023-08-17 13:01:18 -0600

    Adds random fox image to resume

--- resume.js
+++ resume.js
@@ -1,6 +1,19 @@
-fetch('https://randomfox.ca/floof')
-    .then(res => res.json())
-    .then(imagelink => console.log(imagelink))
-    
-    
-    
+async function loadNewImage() {
+  fetch('https://randomfox.ca/floof/').then(async (res) => {
+    const json = await res.json();
+    const element = document.getElementById('img_container');
+    element.src = json.image;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  loadNewImage();
+  // Get a reference to the button element
+  const myButton = document.getElementById('new-img-btn');
+
+  // Add event listener to the button
+  myButton.addEventListener('click', function () {
+    // Code to be executed when the button is clicked
+    loadNewImage();
+  });
+});
